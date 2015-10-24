721a3fdd1638e362e12558f4d9b3a15f4eb5b7a9
Xuan <ytang@pdx.edu>
2015-10-24 08:38:56 -0700

    gonna go shower

--- bookBook/js/controllers/profileController.js
+++ bookBook/js/controllers/profileController.js
@@ -4,11 +4,29 @@ app.controller('profileController', ['$scope', '$state', 'mockService', function
     $scope.userName = $state.params.userName;
     mockService.getUsers().then(function(users){
 
-      $scope.user = mockService.getUserFromName($scope.userName);
+      mockService.getBooks().then(function(){
+        $scope.user = mockService.getUserFromName($scope.userName);
+        $scope.bookList = $scope.user.books.map(function(index){
+          console.log(index);
+          return mockService.getBookByISBN(index);
+        });
+
+        console.log($scope.bookList);
+
+      });
+
+
+
+
+
+
       //console.log(mockService.getUserFromName($scope.userName));
     });
 
-
+    $scope.expand = function(){
+      angular.element(document.querySelectorAll('.sellView')).toggleClass('sideBookViewExpand');
+      console.log("hapend");
+    };
 
 
 }]);
