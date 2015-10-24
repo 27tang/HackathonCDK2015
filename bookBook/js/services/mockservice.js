f4dc964f2a1d3be819f5243131d84d9f6432c2a5
Max Marchuk <max.marchuk@jivesoftware.com>
2015-10-24 02:06:24 -0700

    added functions to add books and users, added underscore

--- bookBook/js/services/mockservice.js
+++ bookBook/js/services/mockservice.js
@@ -1,12 +1,15 @@
 'use strict';
 
 app.service('mockService', function ($http, $q) {
+    var bookList = [];
+    var userList = [];
+
     this.getBooks = function () {
         var def = $q.defer();
         $http.get('data/books.json').success(function (data) {
             def.resolve(data);
+            bookList = _.union(data, bookList);
         });
-
         return def.promise;
     };
 
@@ -14,8 +17,22 @@ app.service('mockService', function ($http, $q) {
         var def = $q.defer();
         $http.get('data/users.json').success(function (data) {
             def.resolve(data);
+            userList = _.union(data, userList);
         });
 
         return def.promise;
     };
+
+    this.addBook = function (book, username) {
+        bookList.push(book);
+        for (var i = 0; i < userList.length; i++){
+            if(userList[i].username == username){
+                userList[i].books.push(book.isbn);
+            }
+        };
+    };
+
+    this.addUser = function (user) {
+        userList.push(user);
+    }
 });
