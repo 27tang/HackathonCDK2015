f8bb4594074d48df85ea3681793e61ca941851f5
Max Marchuk <max.marchuk@jivesoftware.com>
2015-10-23 21:44:59 -0700

    added base angular application and modules

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cdkhackathonApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cdkhackathonApp
+ */
+angular.module('cdkhackathonApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cdkhackathonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cdkhackathonApp
+ */
+angular.module('cdkhackathonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
