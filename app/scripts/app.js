531d724c971f8d81c4d5d650fc3ea86159b7e469
Xuan <ytang@pdx.edu>
2015-10-23 22:26:34 -0700

    pushpush

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,25 +8,30 @@
  *
  * Main module of the application.
  */
-angular
-  .module('cdkhackathonApp', [
-    'ngAnimate',
-    'ngRoute',
-    'ngSanitize'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var app = angular.module('bookBook', ['ngAnimate', 'ui.router', 'mp.autoFocus']);
+
+
+app.config(function($httpProvider){
+  //enable cross domain calls:
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/landing');
+
+  $stateProvider
+
+    .state('landing', {
+      url: '/landing',
+      templateUrl: 'app/views/landing.html',
+      controller: 'landingController'
+    })
+
+    .state('profile', {
+      url: '/profile',
+      templateUrl: 'app/views/profile.html',
+      controller: 'profileController'
+    })
+
+});
